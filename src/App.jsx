ff9aa43ed52a26a60c2f928fdaee1a0f27fc27e8
Banoczi-Csernak Bruno Gyula <bruno@Banoczi-Csernaks-MacBook-Pro.local>
2024-08-12 01:52:57 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Accordian from "./components/accordian";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Accorodian component */}
+      <Accordian />
+    </div>
+  );
+}
+
+export default App;
